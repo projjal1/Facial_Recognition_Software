7aa4376dc8dbcff3a6b6c69fc92f4c5d166ea440
Projjal Gop <gopprojjal@gmail.com>
2020-08-25 11:58:49 +0530

    Update alerts.py
    Added provision for user to add his/her api auth-id

--- alerts.py
+++ alerts.py
@@ -7,7 +7,7 @@ def alert(x):
     payload = "sender_id=FSTSMS&message=Unidentified tried to access system&language=english&route=p&numbers="+x
 
     headers = {
-    'authorization': "3BxnvGSzYmNQETVsZ1k9uOAFHryIKqL4562ghtJfMoe8aiXWwcVIdS9iP705WC8LDEAzenRjYgO1tQpJ",
+    'authorization': "Use your own auth-id here",
     'Content-Type': "application/x-www-form-urlencoded",'Cache-Control': "no-cache",}
 
     response = requests.request("POST", url, data=payload, headers=headers)
