55fe16a6aef49bda371e342cb5f1d26eaf4e2b40
projjal1 <gopprojjal@gmail.com>
2020-11-18 19:56:26 +0530

    Created backend for emotion detections.
    Minor changes: Fixed issue with identify.py and added pycache files to .gitignore

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static/')]
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
